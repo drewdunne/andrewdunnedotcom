ad5be075009e234c6c6298f0bcd416c5e5a375a3
drewdunne <57418947+drewdunne@users.noreply.github.com>
2022-12-02 21:37:39 -0800

    Initializing React Page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component
+{
+    render() {
+        console.log("Rendering App")
+        return (
+            <h1>Coding In Progress</h1>
+        )
+    }
+}
+
+export default App;
